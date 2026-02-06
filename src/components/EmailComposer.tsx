42c7f45fdb59300b2fde25c345e8f293ff24f1e0
cervusDev <gustavo.cervus@gmail.com>
2026-02-06 01:01:23 -0300

    refactor: alteração na composição de email

--- src/components/EmailComposer.tsx
+++ src/components/EmailComposer.tsx
@@ -126,6 +126,7 @@ export function EmailComposer() {
                 <div className="flex justify-end gap-3">
                   <Dialog.Close asChild>
                     <button
+                      onClick={() => form.reset()}
                       type="button"
                       className="
                       px-4 py-2 rounded-lg

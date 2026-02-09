c993831b10a9ccd5a817d06e5001ddc22ef4386c
cervusDev <gustavo.cervus@gmail.com>
2026-02-09 07:02:08 -0300

    renomeando react query

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { router } from "./router/index.tsx";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
-import { TanstackQueryClientProvider } from "./services/reactQuery/index.tsx";
+import { TanstackQueryClientProvider } from "./services/tanstack/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
